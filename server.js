cb454ce3925771247b2efc1daf67da1ff7d7a1d8
RajanMaharjan <razanmaharjan@gmail.com>
2016-02-25 18:44:38 +0545

    added DELET/todo/id

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-// var todoNextId = 1;
 
 
 app.use(bodyParser.json());
@@ -21,35 +20,39 @@ app.get('/todos', function(req, res) {
 	var query = req.query;
 	var where = {};
 
-	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
-	}else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
 		where.completed = false;
 	}
 
-	if(query.hasOwnProperty('q') && query.q.length > 0){
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function(todos){ 
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	},function(e){
+	}, function(e) {
 		res.status(500).json(e);
 	});
 
 });
-	
+
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	
-	db.todo.findById(todoId).then(function(todo){
-		if(!!todo){
+
+	db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
 			res.json(todo.toJSON());
+		} else {
+			res.status(400).send();
 		}
-	},function(e){
-		res.status(404).json(e);
+	}, function(e) {
+		res.status(500).json(e);
 	});
 });
 
@@ -67,18 +70,22 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (!matchedTodo) {
-		res.status(400).json({
-			"error": "No todo found with that id."
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		}else{
+			res.status(204).send();
+		}
+	}, function() {
+		res.status(500).send();
+	});
 });
 
 
